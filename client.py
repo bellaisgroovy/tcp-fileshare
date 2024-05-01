37e27655a27c4e6c4744194c3be66bcd3f637399
bella at work <bella.hodgson@glasgow.ac.uk>
2024-05-01 15:27:54 +0100

    receive put packets

--- client.py
+++ client.py
@@ -3,6 +3,38 @@ import sys
 from utils import RequestType
 
 
+def receive_put_packet(socket):
+    meta_data = socket.recv(516)  # 516 bytes of metadata for all messages (sent to client)
+
+    size = int.from_bytes(meta_data[:40], 'big')
+    request_type = determine_request_type(meta_data[40])
+    filename = meta_data[41:].decode('utf-8')
+
+    payload = bytes(0)
+    data = bytes(1)
+    bytes_read = 0
+    while bytes_read < size - len(meta_data):
+        data = socket.recv(4096)
+
+        payload += data
+        bytes_read += len(data)
+    packet = {
+        'size' : size,
+        'request_type' : request_type,
+        'filename' : filename,
+        'payload' : payload
+    }
+
+    return packet
+
+def determine_request_type(request_bytes):
+    if request_bytes == RequestType.PUT.value:
+        return RequestType.PUT
+    elif request_bytes == RequestType.GET.value:
+        return RequestType.GET
+    if request_bytes == RequestType.LIST.value:
+        return RequestType.LIST
+
 def put_request(filename):
     request_type = RequestType.PUT.value
 
@@ -72,9 +104,17 @@ if request_type == 'list':
     request = list_request()
     cli_sock.sendall(request)
 elif request_type == 'get':
+    # send get request
     path = sys.argv[4]
     request = get_request(path)
     cli_sock.sendall(request)
+
+    # receive put packet
+    packet = receive_put_packet(cli_sock)
+
+    # write payload to file
+    with open(path, 'wb') as file:
+        file.write(packet['payload'])
 elif request_type == 'put':
     path = sys.argv[4]
     request = put_request(path)
